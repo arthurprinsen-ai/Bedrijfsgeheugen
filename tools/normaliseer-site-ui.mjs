2edc7229d1c3dca4f1091ec7e15ec8ee2039487e
arthurprinsen-ai <arthur.prinsen@gmail.com>
2026-09-08 16:47:17 +0200

    Fix production SEO link semantics

--- tools/normaliseer-site-ui.mjs
+++ tools/normaliseer-site-ui.mjs
@@ -13,14 +13,12 @@ import { applyHomepageContextSliderReadability } from './site-shell/fix-homepage
 
 const ORIGIN = 'https://www.bedrijfsgeheugen.nl';
 
-function absolutiseerInterneHref(html) {
-  return String(html).replace(/href=(['"])\/(?!\/)([^'"]*)\1/gi, (_heel, quote, pad) => `href=${quote}${ORIGIN}/${pad}${quote}`);
-}
-
 function herstelTechnischeLinks(html) {
   return String(html)
     .replaceAll(`${ORIGIN}/wachtwoord-vergeten`, `${ORIGIN}/inloggen`)
-    .replaceAll(`${ORIGIN}/blog/afas-koppeling/`, `${ORIGIN}/afas-koppeling`);
+    .replaceAll(`${ORIGIN}/blog/afas-koppeling/`, `${ORIGIN}/afas-koppeling`)
+    .replaceAll('href="/wachtwoord-vergeten"', 'href="/inloggen"')
+    .replaceAll('href="/blog/afas-koppeling/"', 'href="/afas-koppeling"');
 }
 
 function verwijderDefecteLegacyDemonstrator(input) {
@@ -128,7 +126,6 @@ export function normaliseerHtml(input, bestand) {
   html = ensureBrandShellCss(html);
   html = ensureReleaseMarker(html);
   html = markPageSlots(html);
-  html = absolutiseerInterneHref(html);
   html = herstelTechnischeLinks(html);
   html = applyHomepageContextSliderReadability(html);
   return html;
