c7606097ff67cd05a61c488e9caf52becffa4f2a
arthurprinsen-ai <arthur.prinsen@gmail.com>
2026-09-09 07:55:01 +0200

    fix: preserve Portal V2 HTML escaping while integrating builder

--- portal-v2/page-shell.js
+++ portal-v2/page-shell.js
@@ -100,7 +100,7 @@ export function closePortalPage(){
 }
 
 function esc(value=''){
-  return String(value).replace(/[&<>"']/g,ch=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#39;'}[ch]));
+  return String(value).replace(/[&<>"']/g,ch=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#39;'}[ch]));
 }
 
 function renderMetrics(block){
