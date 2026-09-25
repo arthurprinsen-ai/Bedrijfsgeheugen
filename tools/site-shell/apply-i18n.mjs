9f5480239c2af0edaa10f62be5b49ef8cd35667e
arthurprinsen-ai <arthur.prinsen@gmail.com>
2026-09-25 18:36:44 +0200

    fix(i18n): preserve mobile injection idempotence with existing assets

--- tools/site-shell/apply-i18n.mjs
+++ tools/site-shell/apply-i18n.mjs
@@ -21,7 +21,7 @@ function injectMobileLanguage(html) {
 
   if (/id=(["'])bgkopMob\1/i.test(html) || /class=(["'])[^"']*\bbgkop-mob\b[^"']*\1/i.test(html)) {
     const cta = /<a\b[^>]*class=(["'])[^"']*\bbgkop-mcta\b[^"']*\1/i;
-    if (cta.test(html)) return html.replace(cta, MOBILE_LANGUAGE + '$&');
+    if (cta.test(html)) return html.replace(cta, match => MOBILE_LANGUAGE + match);
   }
 
   return html;
@@ -36,9 +36,11 @@ function walk(dir) {
 }
 function patch(file) {
   let html = fs.readFileSync(file,'utf8');
-  if (!/<html\b/i.test(html) || /data-bg-i18n-asset/.test(html)) return;
-  if (/<\/head>/i.test(html)) html = html.replace(/<\/head>/i, LINK + '\n' + SCRIPT + '\n</head>');
-  else return;
+  if (!/<html\b/i.test(html)) return;
+  if (!/data-bg-i18n-asset/.test(html)) {
+    if (/<\/head>/i.test(html)) html = html.replace(/<\/head>/i, LINK + '\n' + SCRIPT + '\n</head>');
+    else return;
+  }
   html = injectMobileLanguage(html);
   fs.writeFileSync(file,html);
 }
